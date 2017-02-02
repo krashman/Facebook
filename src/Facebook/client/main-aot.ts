4b00d358a5c599b7364bb8f753864ca15ba93a99
leolorenzoluis <leolorenzoluis@outlook.com>
2017-02-02 03:23:09 +0000

    Added aot and prod config.

--- src/Facebook/client/main-aot.ts
+++ src/Facebook/client/main-aot.ts
@@ -1,11 +1,11 @@
-// // Entry point for AoT compilation.
+// Entry point for AoT compilation.
 
-// declare var System: any;
+declare var System: any;
 
-// import { platformBrowser } from '@angular/platform-browser';
-// import { enableProdMode } from '@angular/core';
-// import { AppModuleNgFactory } from '../aot/client/app/app.module.ngfactory';
+import { platformBrowser } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
+import { AppModuleNgFactory } from '../aot/client/app/app.module.ngfactory';
 
-// enableProdMode();
+enableProdMode();
 
-// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
