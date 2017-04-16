a131c1bce4692ec49af40349b10f6312ded9a15b
leolorenzoluis <leolorenzoluis@outlook.com>
2017-04-16 18:46:43 -0400

    Fixing build

--- src/Facebook.WebApplication/client/environments/environment.prod.ts
+++ src/Facebook.WebApplication/client/environments/environment.prod.ts
@@ -1,3 +1,21 @@
+
+let IDENTITY_PROVIDER_HOST = 'http://localhost:5001'
 export const environment = {
-  production: true
+  production: true,
+
+  TOKEN_ENDPOINT: `${IDENTITY_PROVIDER_HOST}/connect/token`,
+  REVOCATION_ENDPOINT: `${IDENTITY_PROVIDER_HOST}/connect/revocation`,
+  USERINFO_ENDPOINT: `${IDENTITY_PROVIDER_HOST}/connect/userinfo`,
+
+  CLIENT_ID: "Facebook",
+
+  /**
+   * Resource Owner Password Credential grant.
+   */
+  GRANT_TYPE: "password",
+
+  /**
+   * The Web API, refresh token (offline_access) & user info (openid profile roles).
+   */
+  SCOPE: "WebAPI offline_access openid profile roles"
 };
