2da6c17f1b8904796ee8e055d8ec35c36e130496
leolorenzoluis <leolorenzoluis@outlook.com>
2017-04-16 21:08:55 -0400

    Removing unnecessary assemblies

--- src/Facebook.IdentityProvider/Controllers/ValuesController.cs
+++ src/Facebook.IdentityProvider/Controllers/ValuesController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Facebook.IdentityProvider.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize(Policy = "Access Resources")]
     public class ValuesController : Controller
     {
         // GET api/values
--- src/Facebook.WebApplication/Controllers/UsersController.cs
+++ src/Facebook.WebApplication/Controllers/UsersController.cs
@@ -12,59 +12,67 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Facebook.WebApplication.Controllers
 {
-    [Route("api/[controller]")]
-    public class UsersController : Controller
+  [Route("api/[controller]")]
+  public class UsersController : Controller
+  {
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly IUserRepository _userRepository;
+
+    public UsersController(IUserRepository userRepository, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly IUserRepository _userRepository;
+      _userRepository = userRepository;
+      _signInManager = signInManager;
+      _userManager = userManager;
+    }
 
-        public UsersController(IUserRepository userRepository, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
-        {
-            _userRepository = userRepository;
-            _signInManager = signInManager;
-            _userManager = userManager;
-        }
+    [HttpGet]
+    public IActionResult Get()
+    {
+      var model = new { Email = "", Password = "", RememberMe = false };
+      var result = _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+      if (result.Result.Succeeded)
+      {
+        return NotFound();
+      }
+      return Ok();
+    }
 
-        [HttpGet]
-        public IActionResult Get()
-        {
-            var model = new { Email = "", Password = "", RememberMe = false };
-            var result = _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
-            if (result.Result.Succeeded)
-            {
-                return NotFound();
-            }
-            return Ok();
-        }
-        
-        [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Post([FromBody] User model)
-        {
-            if (!ModelState.IsValid) return BadRequest();
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<IActionResult> Post([FromBody] User model)
+    {
+      if (!ModelState.IsValid) return BadRequest();
 
-            var user = new IdentityUser {UserName = model.Email, Email = model.Email};
-            var result = await _userManager.CreateAsync(user, model.Password);
+      var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+      var result = await _userManager.CreateAsync(user, model.Password);
+      if (!result.Succeeded) return new JsonResult(result);
 
-            return new JsonResult(result);
-            
-            // Assigns claims.
-            //var claims = new List<Claim>
-            //{
-            //    new Claim(type: JwtClaimTypes.GivenName, value: "hello"),
-            //    new Claim(type: JwtClaimTypes.FamilyName, value: "world"),
-            //};
 
-            //await _userManager.AddClaimsAsync(user, claims);
-            // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
-            // Send an email with this link
-            //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            //var callbackUrl = Url.Action(nameof(ConfirmEmail), "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
-            //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
-            //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
-            //await _signInManager.SignInAsync(user, isPersistent: false);
-        }
-        
-    }  
+      await _userManager.AddToRoleAsync(user, "user");
+      var claims = new List<Claim> {
+            new Claim(type: JwtClaimTypes.GivenName, value: model.FirstName),
+            new Claim(type: JwtClaimTypes.FamilyName, value: model.LastName)
+        };
+      await _userManager.AddClaimsAsync(user, claims);
+      return new JsonResult(result);
+
+      // Assigns claims.
+      //var claims = new List<Claim>
+      //{
+      //    new Claim(type: JwtClaimTypes.GivenName, value: "hello"),
+      //    new Claim(type: JwtClaimTypes.FamilyName, value: "world"),
+      //};
+
+      //await _userManager.AddClaimsAsync(user, claims);
+      // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
+      // Send an email with this link
+      //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+      //var callbackUrl = Url.Action(nameof(ConfirmEmail), "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+      //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
+      //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
+      //await _signInManager.SignInAsync(user, isPersistent: false);
+    }
+
+  }
 }
