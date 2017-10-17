bc0239ca9fbc83f7d0b035459384f51dc96ddf62
leolorenzoluis <leolorenzoluis@outlook.com>
2017-10-17 00:59:20 -0400

    Try v2

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then rvm --default use ruby-2.3.3; fi
 if [ -n "${DOTNETCORE}" ]; then dotnet restore && dotnet build ./Facebook.sln; fi
 
 cd src/Facebook.WebApplication
